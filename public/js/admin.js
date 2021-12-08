43e63c7745915334c44ac6b7192cf5b185ee51c9
rachna-eng <rachshah87@gmail.com>
2021-12-08 01:49:14 -0500

    Profile picture upload, ajax,route,formlauout changed

---
+++ public/js/admin.js
@@ -0,0 +1,39 @@
+(function ($) {
+  $("#createprod").submit(function (event) {
+    event.preventDefault();
+    var formData = new FormData();
+    formData.append("image", $("#imageUpload")[0].files[0]);
+    $.ajax({
+      type: "POST",
+      url: "/uploadSingle",
+      contentType: false,
+      data: formData,
+      processData: false,
+      success: function (path) {
+        const newProd = {
+          productName: $("#productName").val(),
+          productLinks: $("#productLinks").val().split(","),
+          brand: $("#brand").val(),
+          price: $("#price").val(),
+          category: $("#category").val(),
+          pendingid: $("#pendingid").val(),
+          productPicture: path,
+        };
+        $.ajax({
+          type: "POST",
+          url: "/products",
+          data: newProd,
+          success: function (response) {
+            window.location.href = "/products";
+          },
+          error: function (error) {
+            alert(error.responseText);
+          },
+        });
+      },
+      error: function (error) {
+        alert(error.responseText);
+      },
+    });
+  });
+})(window.jQuery);
